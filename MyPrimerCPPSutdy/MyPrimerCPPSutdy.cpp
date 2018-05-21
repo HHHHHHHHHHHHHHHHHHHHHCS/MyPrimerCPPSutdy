b6326d47f5f3482c987a2e57651c0366f6b1e560
HCS <464962683@qq.com>
2018-05-22 00:48:29 +0800

    第一个测试案例添加

--- MyPrimerCPPSutdy/MyPrimerCPPSutdy.cpp
+++ MyPrimerCPPSutdy/MyPrimerCPPSutdy.cpp
@@ -3,9 +3,17 @@
 
 #include "stdafx.h"
 
+#include <iostream>  
+using namespace std;
 
 int main()
 {
-    return 0;
+	cout << "Enter two number:" << endl;
+	int v1 = 0, v2 = 0;
+	cin >> v1 >> v2;
+	cout << "The sum of " << v1 << " and " << v2
+		<< " is " << v1 + v2 << endl;
+	system("pause");
+	return 0;
 }
 
