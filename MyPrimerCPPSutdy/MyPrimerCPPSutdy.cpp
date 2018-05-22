03807b52c752becb97b4404e6d8a84791530ba35
HCS <464962683@qq.com>
2018-05-23 00:53:11 +0800

    做了一下练习题  留了一个BUG 待解决

--- MyPrimerCPPSutdy/MyPrimerCPPSutdy.cpp
+++ MyPrimerCPPSutdy/MyPrimerCPPSutdy.cpp
@@ -2,17 +2,17 @@
 //
 
 #include "stdafx.h"
-
 #include <iostream>  
+
+#include "_01_02_HelloWorld.cpp"
+
 using namespace std;
 
 int main()
 {
-	cout << "Enter two number:" << endl;
-	int v1 = 0, v2 = 0;
-	cin >> v1 >> v2;
-	cout << "The sum of " << v1 << " and " << v2
-		<< " is " << v1 + v2 << endl;
+	_01_02_HelloWorld hw =  _01_02_HelloWorld();
+	hw.Test();
+
 	system("pause");
 	return 0;
 }
---
+++ MyPrimerCPPSutdy/_01_02_HelloWorld.cpp
@@ -0,0 +1,58 @@
+#include "stdafx.h"
+#include <iostream>  
+using namespace std;
+
+class _01_02_HelloWorld
+{
+public:
+	void Test();
+	void CoutHelloWorld();
+	void CoutSumByNumber();
+	void CoutMulByNumber();
+	void CoutSumWithLine();
+private:
+};
+
+void _01_02_HelloWorld::Test()
+{
+	CoutHelloWorld();
+	cout << "-------------------------" << endl;
+	CoutSumByNumber();
+	cout << "-------------------------" << endl;
+	CoutMulByNumber();
+	cout << "-------------------------" << endl;
+	CoutSumWithLine();
+	cout << "-------------------------" << endl;
+}
+
+void _01_02_HelloWorld::CoutHelloWorld()
+{
+	cout << "Hello World" << endl;
+}
+
+void _01_02_HelloWorld::CoutSumByNumber()
+{
+	cout << "Enter two number:" << endl;
+	int v1 = 0, v2 = 0;
+	cin >> v1 >> v2;
+	cout << "The sum of " << v1 << " and " << v2
+		<< " is " << v1 + v2 << endl;
+}
+
+void _01_02_HelloWorld::CoutMulByNumber()
+{
+	cout << "Enter two number:" << endl;
+	int v1 = 0, v2 = 0;
+	cin >> v1 >> v2;
+	cout << "The sum of " << v1 << " Mul " << v2
+		<< " is " << v1 * v2 << endl;
+}
+
+void _01_02_HelloWorld::CoutSumWithLine()
+{
+	cout << "Enter two number:" << endl;
+	int v1 = 0, v2 = 0;
+	cin >> v1 >> v2;
+	cout << "The sum of " << endl << v1 << endl << " Mul " << endl << v2
+		<< endl << " is " << endl << v1 * v2 << endl;
+}
