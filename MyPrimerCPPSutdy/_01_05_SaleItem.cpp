0af739f29d2fa05f22118483b676b511415de802
HCS <464962683@qq.com>
2018-05-30 00:29:46 +0800

    完成了一下作业

--- MyPrimerCPPSutdy/_01_05_SaleItem.cpp
+++ MyPrimerCPPSutdy/_01_05_SaleItem.cpp
@@ -28,15 +28,53 @@ void _01_05_SaleItem::Add()
 
 void _01_05_SaleItem::ReadSome()
 {
+	int id;
+	float price;
+	while (cin >> id)
+	{
+		cin >> price;
 
+		cout << id << "____" << price << endl;
+	}
 }
 
 void _01_05_SaleItem::AddTwoSame()
 {
-
+	int id1, id2;
+	float price1, price2;
+	while (cin >> id1 >> price1 >> id2 >> price2 )
+	{
+		if (id1 == id2)
+		{
+			cout << id1 << " sum :" << price1 + price2 << endl;
+		}
+		else
+		{
+			cout << id1 << " != " << id2 << endl;
+		}
+	}
 }
 
 void _01_05_SaleItem::AddSomeSame()
 {
+	int id1, id2;
+	float price, sumPrice=0;
+	if (cin >> id1 >> price)
+	{
+		sumPrice += price;
+		while (cin >> id2 >> price)
+		{
+			if (id1 == id2)
+			{
+				sumPrice += price;
+			}
+			else
+			{
+				cout << id1 << " sum :" << sumPrice << endl;
+				sumPrice = price;
+				id1 = id2;
+			}
+		}
 
+	}
 }
